b4ea72e70d5c69ef97c555ce4080a90204d4e354
Toni Kleinfeld <54000942+gaunerleechy@users.noreply.github.com>
2024-11-22 15:26:14 +0100

    create draw class function to schuffle cards array and drawing diffent cards from stack

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,6 +1,13 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+import { Game } from '../../models/game';
 
+interface GameObject {
+    readonly players: string[],
+    stack:string[],
+    playedCard:string[],
+    currentPlayer:number,
+}
 @Component({
   selector: 'app-game',
   standalone: true,
@@ -8,10 +15,29 @@ import { Component } from '@angular/core';
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
+
 export class GameComponent {
-  pickCardAnimation = false;
+   pickCardAnimation:boolean = false; 
+   game:GameObject|undefined;
+   currentCard:string|undefined = '';
+
+   ngOnInit(){
+    this.newGame()
+   }
+
+  newGame(){
+    this.game = new Game()
+    console.log(this.game)
+  }
+
 
   takeCard(){
-    this.pickCardAnimation = true
+    if (!this.pickCardAnimation && this.game ) {
+      this.currentCard = this.game.stack.pop();          
+      this.pickCardAnimation = true;   
+
+      setTimeout(() => {this.pickCardAnimation = false;},2000
+      )
+    }  
   }
 }
