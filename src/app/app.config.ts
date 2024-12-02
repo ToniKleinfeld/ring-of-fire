4a6b046e6df9f1da4e17cf8b0411bbb1f674b54a
Toni Kleinfeld <54000942+gaunerleechy@users.noreply.github.com>
2024-12-02 12:25:15 +0100

    add agnular-firebase - changed project id to lower-case

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,12 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync()]
+  providers: [provideRouter(routes), provideAnimationsAsync(), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"ringoffire-12b13","appId":"1:372744712056:web:880c724d893afa5083e28d","storageBucket":"ringoffire-12b13.firebasestorage.app","apiKey":"AIzaSyBm2zyqSWcda0Jr671tpV9zuVthojCWySY","authDomain":"ringoffire-12b13.firebaseapp.com","messagingSenderId":"372744712056"}))), importProvidersFrom(provideDatabase(() => getDatabase())), importProvidersFrom(provideStorage(() => getStorage()))]
 };
